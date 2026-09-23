94d929443dcb6b7457303a2cf3592f1fdbb2d5a4
nopalodon <nflcoding@gmail.com>
2026-09-23 09:26:03 +0700

    add tab layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         viewBinding = true
     }
@@ -40,6 +41,10 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    implementation ("com.google.android.material:material:1.3.1")
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/mycourse/MainActivity.kt
+++ app/src/main/java/com/example/mycourse/MainActivity.kt
@@ -4,16 +4,35 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+// Tab Layout
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     // 1. Method onCreate untuk memuat layout utama
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1. Inisialisasi adapter ViewPager2
+//        otak dari pager
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+//        tempat pager akan di load
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+//        dimana tab akan di load
+        viewPager.adapter = sectionsPagerAdapter
+
+        // 2. Hubungkan TabLayout dengan ViewPager2 menggunakan TabLayoutMediator
+        val tabs: TabLayout = findViewById(R.id.tab_layout)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        // Menghilangkan bayangan/elevation di bawah ActionBar jika ada
+        supportActionBar?.elevation = 0f
     }
     // 2. Method untuk menampilkan Options Menu
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,4 +58,15 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3
+        )
+    }
+
+
 }
